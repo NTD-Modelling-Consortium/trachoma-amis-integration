c45d9c0ddee39411ef0bf6d960bcc5dceac8ed26
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-08-13 18:12:44 +0100

    Add some more tests demonstrating issues with random

--- tests/test_amis_integration.py
+++ tests/test_amis_integration.py
@@ -10,4 +10,32 @@ def test_build_transmission_model():
     result = transmission_model([1], [0.2], 0)
     # At the moment the seed is ignored so this is different each time
     # hence the wide margin on matching
-    npt.assert_almost_equal(result, np.array([[0], [0.5]]), decimal=1)
+    npt.assert_almost_equal(result, np.array([[0, 0.5]]), decimal=1)
+
+
+def test_running_twice_with_different_seed_produces_different_result_at_first_week():
+    transmission_model = amis_integration.build_transmission_model(
+        fitting_points=[1], initial_infect_frac=0.5, num_cores=1
+    )
+    result = transmission_model(seeds=[1, 2], params=[0.2, 0.2], n_tims=0)
+    # TODO: this currently fails because the setup is shared between
+    # each of the runs so at 1 week they are all in sync
+    assert result[0][0] != result[1][0]
+
+
+def test_running_twice_with_different_seed_produces_different_result_after_10_weeks():
+    transmission_model = amis_integration.build_transmission_model(
+        fitting_points=[10], initial_infect_frac=0.5, num_cores=1
+    )
+    result = transmission_model(seeds=[1, 2], params=[0.2, 0.2], n_tims=0)
+    # TODO: I don't understand why this works, as far as I can tell each run
+    # uses the same copied state for each run
+    assert result[0][0] != result[1][0]
+
+
+def test_running_twice_with_same_seed_produces_same_result_after_10_weeks():
+    transmission_model = amis_integration.build_transmission_model(
+        fitting_points=[10], initial_infect_frac=0.5, num_cores=1
+    )
+    result = transmission_model(seeds=[1, 1], params=[0.2, 0.2], n_tims=0)
+    assert result[0][0] == result[1][0]
