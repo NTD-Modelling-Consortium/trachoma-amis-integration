418a23dc65dcb00d66315270a7b98f6b4488eb19
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-08-19 12:14:52 +0100

    Fix tests to pass two params
    The model now has a parameter for coverage so that needs to
    be passed in.

--- tests/test_amis_integration.py
+++ tests/test_amis_integration.py
@@ -8,7 +8,7 @@ def test_build_transmission_model():
     transmission_model = amis_integration.build_transmission_model(
         fitting_points=[0, 1], initial_infect_frac=0.5, num_cores=1
     )
-    result = transmission_model([1], [0.2], 0)
+    result = transmission_model([1], [(0.2, 0.4)], 0)
     # At the moment the seed is ignored so this is different each time
     # hence the wide margin on matching
     npt.assert_almost_equal(result, np.array([[0, 0.5]]), decimal=1)
@@ -19,7 +19,7 @@ def test_running_twice_with_different_seed_produces_different_result_at_first_we
     transmission_model = amis_integration.build_transmission_model(
         fitting_points=[1], initial_infect_frac=0.5, num_cores=1
     )
-    result = transmission_model(seeds=[1, 2], params=[0.2, 0.2], n_tims=0)
+    result = transmission_model(seeds=[1, 2], params=[(0.2, 0.4), (0.2, 0.4)], n_tims=0)
     assert result[0][0] != result[1][0]
 
 
@@ -30,7 +30,7 @@ def test_running_twice_with_different_seed_produces_different_result_after_10_we
     transmission_model = amis_integration.build_transmission_model(
         fitting_points=[10], initial_infect_frac=0.5, num_cores=2
     )
-    result = transmission_model(seeds=[1, 2], params=[0.2, 0.2], n_tims=0)
+    result = transmission_model(seeds=[1, 2], params=[(0.2, 0.4), (0.2, 0.4)], n_tims=0)
     assert result[0][0] != result[1][0]
 
 
@@ -38,7 +38,7 @@ def test_running_twice_with_different_seed_produces_different_result_after_10_we
     transmission_model = amis_integration.build_transmission_model(
         fitting_points=[10], initial_infect_frac=0.5, num_cores=1
     )
-    result = transmission_model(seeds=[1, 2], params=[0.2, 0.2], n_tims=0)
+    result = transmission_model(seeds=[1, 2], params=[(0.2, 0.4), (0.2, 0.4)], n_tims=0)
     # This works only becuase the single core means the state is shared from one run to the next
     assert result[0][0] != result[1][0]
 
@@ -48,5 +48,5 @@ def test_running_twice_with_same_seed_produces_same_result_after_10_weeks():
     transmission_model = amis_integration.build_transmission_model(
         fitting_points=[10], initial_infect_frac=0.5, num_cores=1
     )
-    result = transmission_model(seeds=[1, 1], params=[0.2, 0.2], n_tims=0)
+    result = transmission_model(seeds=[1, 1], params=[(0.2, 0.4), (0.2, 0.4)], n_tims=0)
     assert result[0][0] == result[1][0]
