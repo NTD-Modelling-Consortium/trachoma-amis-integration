9c1bc5fc1fc2bbd158641f9041b232686979ee29
Raiha Browning <raiha_p123@hotmail.com>
2024-10-18 11:31:39 +0100

    update fitting script

--- trachoma_fitting.R
+++ trachoma_fitting.R
@@ -1,41 +1,99 @@
+
+# on cluster:
+#id = as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+# for testing:
+id = 556
+library(dplyr)
 library(reticulate)
+
+# load trachoma model 
+# first 3 lines only necessary on local machine - on cluster will install R package, set working dir. and source venv before running script
+library(AMISforInfectiousDiseases)
+setwd("~/Documents/trachoma-endgame/trachoma-amis-integration/")
+reticulate::use_virtualenv("~/Documents/trachoma-endgame/trachoma-venv", required=TRUE)
 amis_int_mod <- import("trachoma_amis")
 
-# Example prevalence map, with two locations, fitting to two time times
-# Both locations start at 0.031, and the second time point is 0.021
-prevalence_map <- vector("list", 2)
-prevalence_map[[1]]$data <- matrix(c(0.031, 0.031))
-prevalence_map[[2]]$data <- matrix(c(0.021, 0.021))
+# Load prevalence map and filter rows for TaskID == id
+load(paste0("../Maps/trachoma_maps.rds"))
+load("../Maps/trachoma_map_years.rds")
+prevalence_map = trachoma_maps
+prevalence_map = lapply(1:length(prevalence_map), function(t){
+  output=list(data = as.matrix(prevalence_map[[t]]$data %>% 
+                                                  filter(TaskID==id) %>% 
+                                                  select(max_tf_prev_upr),ncol=1), 
+              likelihood = prevalence_map[[t]]$likelihood)
+  rownames(output$data) = prevalence_map[[t]]$data$IU_ID[prevalence_map[[t]]$data$TaskID==id]
+  return(output)
+})
+# just get maps with data for this batch
+map_bool_id = which(sapply(1:length(prevalence_map), function(t) !all(is.na(prevalence_map[[t]]$data))))
+years_vector_id = years_vector[map_bool_id] 
+prevalence_map = lapply(map_bool_id, function(t) prevalence_map[[t]])
 
-#' the "dprior" function
-#' Note the second parameter _must_ be called log
-#' Unclear what this function does
-density_function <- function(parameters, log) {
-    return(0.5)
+# define prior hyperparameters and boundaries
+beta_rate = 2
+beta_lb = 0
+beta_ub = Inf
+
+eff_cov_lb = 0
+eff_cov_ub = 1
+a_eff_cov = 2
+b_eff_cov = 2
+
+# random number generator
+rprior = function(n){
+  params = matrix(NA,ncol=2,nrow=n)
+  params[,1] = rexp(n,rate=beta_rate)
+  params[,2] = rbeta(n,a_eff_cov,b_eff_cov)
+  colnames(params) = c("beta","eff_cov")
+  return(params)
 }
 
-#' The "rprior" function that returns a matrix whose columns are the parameters
-#' and each row is a sample
-rnd_function <- function(num_samples) {
-    # set beta to just be 0.04 for each sample
-    beta <- rep(0.04, num_samples)
-  
-    # set coverage to be randomly generated numbers between 0 and 1
-    coverage <- runif(num_samples, min = 0, max = 1)
+# prior density
+dprior = function(x, log){
+  if(!is.matrix(x)){
+    if (log){
+      d = sum(dexp(x[1],rate=beta_rate,log=T),
+              dbeta(x[2],a_eff_cov,b_eff_cov,log=T))
+    } else {
+      d = prod(dunif(x[1],rate=beta_rate,log=F),
+               dbeta(x[2],a_eff_cov,b_eff_cov,log=F))
+    }
     
-    # Combine the two columns to form the params matrix
-    result <- cbind(beta, coverage)
-    
-    return(result)
-
+  } else {
+    if (log){
+      d = sum(dunif(x[,1],rate=beta_rate,log=T),
+              dbeta(x[,2],a_eff_cov,b_eff_cov,log=T))
+    } else {
+      d = prod(dunif(x[,1],rate=beta_rate,log=F),
+               dbeta(x[,2],a_eff_cov,b_eff_cov,log=F))
+    }
+  }
+  return(d)
 }
 
-prior <- list("dprior" = density_function, "rprior" = rnd_function)
+prior = list(rprior=rprior,dprior=dprior)
 
-#' Fit to initial and last week (in this case 100 years)
-weeks_indices <- c(0L, 5199L)
-initial_infect = 0.01
-num_cores = 1L
+
+# Algorithm parameters
+amis_params<-default_amis_params()
+amis_params$max_iters=10
+amis_params$n_samples=500
+amis_params$target_ess =500
+amis_params$sigma=0.0025
+amis_params$boundaries=c(-Inf,Inf)
+amis_params$boundaries_param = matrix(c(beta_lb,eff_cov_lb,beta_ub,eff_cov_ub),ncol=2)
+
+# shell to save trajectories
+trajectories = c() # save simulated trajectories as code is running
+if (!dir.exists("../trajectories")) {dir.create("../trajectories")}
+save(trajectories,file=paste0("../trajectories/trajectories_",id,".Rdata"))
+
+# Fit to years_vector years (70 years burn in, currently I think maps are at the start of the year/end of previous year since index starts at 0)
+all_years_vector_id = seq(min(years_vector_id),(max(years_vector_id)+1),by=0.25)
+weeks_indices <- as.integer(70*52 + (all_years_vector_id - min(years_vector))*52)
+initial_infect = 0.5
+num_cores = 8L
 
 model_func <- amis_int_mod$build_transmission_model(
     weeks_indices, initial_infect, num_cores
@@ -50,17 +108,55 @@ error_function <- function(e) {
     message(msg)
     stop(print(err))
 }
+# Wrap model_func so as to print the Python traceback
+# in case of a raised exception during execution of the
+# Python model. See
+# https://rstudio.github.io/reticulate/reference/py_last_error.html
+transmission_model = function(seeds, params, n_tims) {
+  # tryCatch(
+  #   expr={
+      output=model_func(seeds, params, n_tims)
+      
+      #save simulated trajectories 
+      load(paste0("../trajectories/trajectories_",id,".Rdata"))
+      trajectories =  rbind(trajectories,output)
+      save(trajectories, file=paste0("../trajectories/trajectories_",id,".Rdata"))
+      
+      output_map_years = output[,which(all_years_vector_id %in% (years_vector_id+0.75))]
+      return(output_map_years)
+      
+  #   },
+  #   error=error_function
+  # )
+}
+#test=transmission_model(1:10,rprior(10),21)
+
+st<-Sys.time()
 amis_output <- AMISforInfectiousDiseases::amis(
     prevalence_map,
-    # Wrap model_func so as to print the Python traceback
-    # in case of a raised exception during execution of the
-    # Python model. See
-    # https://rstudio.github.io/reticulate/reference/py_last_error.html
-    function(seeds, params, n_tims) {
-        tryCatch(
-            expr={model_func(seeds, params, n_tims)},
-            error=error_function
-        )
-    },
-    prior
+    seed=id,
+    amis_params=amis_params,
+    transmission_model=transmission_model,
+    prior,
+    output_dir = paste0("../amis_intermittant_outputs/batch",id,"_")
 )
+en<-Sys.time()
+dur_amis<-as.numeric(difftime(en,st,units="mins"))
+if (!dir.exists("../AMIS_output")) {dir.create("../AMIS_output")}
+save(amis_output,file=paste0("../AMIS_output/amis_output",id,".Rdata"))
+
+
+# Currently errors - I think because I
+# don't know where the weights need to be set
+# "No weight on any particles for locations in the active set."
+print(amis_output)
+
+# save summary
+ess<-amis_output$ess
+n_success<-length(which(ess>=amis_params[["target_ess"]]))
+failures<-which(ess<amis_params[["target_ess"]])
+n_failure<-length(failures)
+if (n_failure>0) {cat(paste(failures,id,ess[failures]),file = paste0("../ESS_NOT_REACHED.txt"),sep = "\n", append = TRUE)}
+if (!file.exists(paste0("../summary.csv"))) {cat("ID,n_failure,n_success,n_sim,min_ess,duration_amis,durarion_subsampling\n",file=paste0("../summary.csv"))}
+cat(id,n_failure,n_success,length(amis_output$seeds),min(ess),dur_amis,NA,"\n",sep=",",file=paste0("../summary.csv"),append=TRUE)
+
