af022c8154a86958f87a86be12655e69aef37426
cakula <chatrapathi.akula@gmail.com>
2025-05-23 15:27:35 +0100

    Include preparation steps before fitting and projections can even start

--- run_historic_simulations.sh
+++ run_historic_simulations.sh
@@ -76,6 +76,10 @@ fi
 # Export task ID for sub-processes
 export SLURM_ARRAY_TASK_ID=$ID
 
+echo "Preparing histories and maps..."
+Rscript prepare_histories_and_maps.R --id="$ID" || exit 1
+Rscript prepare_histories_projections.R --id="$ID" || exit 1
+
 # Run the fitting process
 echo "Running trachoma fitting and preprocessing projections..."
 ./run_fit_and_process.sh --id="$ID" --folder_id="$FOLDER_ID" $COMMON_ARGS || exit 1
